1ba92565d0ad3f9bfe28549328e565bd3157e72e
shaphat-codes <jehoshaphattatiglo99@gmai.com>
2023-06-05 15:44:14 +0000

    first full stack deploymemt- fixed standar import error

--- extractor/non_standard_image_extraction.py
+++ extractor/non_standard_image_extraction.py
@@ -1,4 +1,4 @@
-irom django.shortcuts import render
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import *
